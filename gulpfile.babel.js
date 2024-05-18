97e5a6bc09d2f415782b051dccaa328fb1da6e44
Raymall Perez <raymall.perez@gmail.com>
2024-05-18 00:20:01 -0400

    Add ES6 support for gulpfile

---
+++ gulpfile.babel.js
@@ -0,0 +1,30 @@
+import gulp from 'gulp'
+import { exec } from 'child_process'
+
+const execCommand = (command, message, callbackFn) => {
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(command, error, stderr)
+      return
+    }
+      
+    stdout ? console.log(stdout) : null
+
+    console.log(stdout)
+    console.log(message)
+    callbackFn ? callbackFn() : null
+  })
+}
+
+const typescript = (cb) => {
+  execCommand(
+    `pnpm tsc`,
+    'Compiling...',
+    execCommand(`node dist/update.js`, 'Completed')
+  )
+  cb()
+}
+
+exports.default = function() {
+  gulp.watch('src/**/*.ts', typescript)
+}
